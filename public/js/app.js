38ae2131ba42adb54a3b017d5aec15e01383f0d4
Docas <docas@pkking.ph>
2019-05-29 18:18:02 +0800

    登录成功后导航逻辑

--- public/js/app.js
+++ public/js/app.js
@@ -49437,8 +49437,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/docas/www/blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/docas/www/blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\TestCode\blog\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\TestCode\blog\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
